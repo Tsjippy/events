1f06631795406e226febcfd5abcaa198157f751b
Tsjippy <ewaldha@hotmail.com>
2024-10-04 10:28:03 +0100

    Initial commit

---
+++ blocks/metadata/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '26277afe15dc2e9b3dd9');
---
+++ blocks/upcomingArrivals/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e9c2ca54cdb835798463');
