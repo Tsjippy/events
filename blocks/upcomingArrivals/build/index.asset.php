00e50bd0391fa2ddfc2311325aa6c8fc9609e00e
Tsjippy <ewaldha@hotmail.com>
2024-10-04 12:38:18 +0100

    changed: upgrade deps

--- blocks/upcomingArrivals/build/index.asset.php
+++ blocks/upcomingArrivals/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e9c2ca54cdb835798463');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b8a5a80ea64b106f8c43');
--- blocks/upcomingEvents/build/index.asset.php
+++ blocks/upcomingEvents/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3b3d0e243460a9ecb247');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0bd8ea67d0e3db7f52a2');
