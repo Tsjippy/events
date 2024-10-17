08a64c9ace325f7ac66fb4508cb475b9b7e2ace4
Tsjippy <ewaldha@hotmail.com>
2024-10-17 11:59:40 +0100

    updated: blocks

--- blocks/metadata/build/index.asset.php
+++ blocks/metadata/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bf0d9ec094cb42cc8338');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6a1c74faa3858fc5d6c4');
--- blocks/schedules/build/index.asset.php
+++ blocks/schedules/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd31108e0dcd984ca1d81');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9994c0ae99e9c34967d');
