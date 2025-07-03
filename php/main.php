b63611d5c3439f7eae3c184ecb84999073660609
Tsjippy <ewaldha@hotmail.com>
2025-07-03 12:57:01 +0100

    skip:

--- php/main.php
+++ php/main.php
@@ -30,24 +30,3 @@ function editButton($buttonHtml, $post, $content){
 
     return $buttonHtml;
 }
-
-// Do not display single events in search results
-/**
- * This function modifies the main WordPress query to remove 
- * pages from search results.
- *
- * @param object $query The main WordPress query.
- */
-function tg_exclude_pages_from_search_results( $query ) {
-    if ( $query->is_main_query() && $query->is_search() && ! is_admin() ) {
-        $query->set( 'post_type', array( 'post' ) );
-    }    
-}
-add_action( 'pre_get_posts', __NAMESPACE__.'\tg_exclude_pages_from_search_results' );
-
-add_action('loop_start', __NAMESPACE__.'\loopStart');
-function loopStart($query){
-	if($query->is_main_query() && is_search()){
-		ob_start();
-	}
-}
