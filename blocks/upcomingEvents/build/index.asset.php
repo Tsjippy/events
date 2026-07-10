05adcd56d18c0fa6a22aee6e7f004e23ad7cad0e
Tsjippy <ewaldha@hotmail.com>
2026-07-10 09:15:04 +0200

    added: block icon

--- blocks/upcomingEvents/build/index.asset.php
+++ blocks/upcomingEvents/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '49fddd32f2ca90717871');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd44294d61c5e3d3526f');
