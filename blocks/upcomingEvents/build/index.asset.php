a37f221531494a6429e86afc5d1f788476d1b671
Tsjippy <ewaldha@hotmail.com>
2026-07-10 21:44:44 +0200

    changed: hook name

--- blocks/upcomingEvents/build/index.asset.php
+++ blocks/upcomingEvents/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd129aa8baff7a433ff9a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '822329a7fbffaa011111');
